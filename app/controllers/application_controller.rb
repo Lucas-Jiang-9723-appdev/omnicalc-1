e796ece5425cce8589517be0e5b79b20c239a88e
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-07-04 21:07:40 +0000

    1 commit - square done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
 
+  def square_calc
+    render({:template => "calculation_temp/square.html.erb" })
+  end
+
+  def square_result
+    @input = params.fetch("input_num")
+    @squared_input = @input.to_f**2
+    render({:template => "calculation_temp/square_result.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-
+get("/square/new", {:controller => "application", :action => "square_calc"})
+get("/square/result", {:controller => "application", :action => "square_result"})
+# get("", {:controller => "", :action => ""})
 end
