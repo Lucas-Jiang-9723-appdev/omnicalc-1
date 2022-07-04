56219a3cda4d2317989f0131ad901c22d1b68394
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-07-04 21:23:53 +0000

    2nd commit - sqrt completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,15 @@ class ApplicationController < ActionController::Base
     @squared_input = @input.to_f**2
     render({:template => "calculation_temp/square_result.html.erb"})
   end
+
+  def square_root_calc
+    render({:template => "calculation_temp/square_root.html.erb"})
+  end
+
+  def square_root_result
+    @input = params.fetch("input_num")
+    @square_rooted_input = @input.to_f**0.5
+    render({:template => "calculation_temp/square_root_result.html.erb"})
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 get("/square/new", {:controller => "application", :action => "square_calc"})
 get("/square/result", {:controller => "application", :action => "square_result"})
-# get("", {:controller => "", :action => ""})
+get("/square_root/new", {:controller => "application", :action => "square_root_calc"})
+get("/square_root/result", {:controller => "application", :action => "square_root_result"})
+# get("", {:controller => "application", :action => ""})
 end
